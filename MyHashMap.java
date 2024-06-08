1e65474d04c8f72e5aee960e5643dc971db7697c
DhimasFauzi11 <148042479+DhimasFauzi11@users.noreply.github.com>
2024-06-08 20:41:47 +0700

    Update MyHashMap.java

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,40 +1,41 @@
 public class MyHashMap<K, V> {
 
-    private class masuk<K, V> {
-      private K key;
-      private V value;
-      private masuk<K, V> next;
-  
-      public masuk(K key, V value) {
-        this.key = key;
-        this.value = value;
-      }
-  
-      public K getKey() {
-        return this.key;
-      }
-  
-      public V getValue() {
-        return this.value;
-      }
-  
-      public void setValue(V value) {
-        this.value = value;
-      }
-  
-      @Override
-      public String toString() {
-        masuk<K, V> temp = this;
-        StringBuilder sb = new StringBuilder();
-        while (temp != null) {
-          sb.append(temp.key + " -> " + temp.value + ",");
-          temp = temp.next;
-        }
-        return sb.toString();
-      }
-  
+  private class masuk<K, V> {
+    private V value;
+    private K key;
+    private masuk<K, V> next;
+
+    public K getKey() {
+      return this.key;
     }
-     private final int SIZE = 5000;
+
+    public masuk(K key, V value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    public void setValue(V value) {
+      this.value = value;
+    }
+
+    public V getValue() {
+      return this.value;
+    }
+
+    @Override
+    public String toString() {
+      masuk<K, V> temp = this;
+      StringBuilder sb = new StringBuilder();
+      while (temp != null) {
+        sb.append(temp.key + " -> " + temp.value + ",");
+        temp = temp.next;
+      }
+      return sb.toString();
+    }
+
+  }
+
+  private final int SIZE = 5000;
 
   private masuk<K, V> table[];
 
@@ -65,7 +66,8 @@ public class MyHashMap<K, V> {
       e.next = new masuk<K, V>(key, value);
     }
   }
-    public V get(K key) {
+
+  public V get(K key) {
     int hash = key.hashCode() % SIZE;
     masuk<K, V> e = table[hash];
 
@@ -96,6 +98,7 @@ public class MyHashMap<K, V> {
       e.next = null;
       return e;
     }
+
     masuk<K, V> prev = e;
     e = e.next;
 
@@ -125,5 +128,4 @@ public class MyHashMap<K, V> {
 
     return sb.toString();
   }
-}  
-
+}
